7150e7c254517e834ea07329024a221d83dcce1e
Hugo Nery <hugoalbuquerquen@gmail.com>
2025-03-11 10:58:25 +0000

    Organizing project folder n' files

---
+++ MovieGeeky/themoviegeek.py
@@ -0,0 +1,7 @@
+import os
+class TheMovieGeek:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
+    text = input("Type in the begining of the movie genre you'd like recommendations: ")
+
+    print(text)
